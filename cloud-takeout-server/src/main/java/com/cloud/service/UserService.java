7cf19ac06dd5e9d868d10bbeecfb7c180a3fe531
gengzeyu <2695917656@qq.com>
2023-11-26 17:38:52 +0800

    first

--- cloud-takeout-server/src/main/java/com/cloud/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.cloud.service;
-
-import com.cloud.dto.UserLoginDTO;
-import com.cloud.vo.UserLoginVO;
-
-public interface UserService {
-    UserLoginVO login(UserLoginDTO userLoginDTO);
-}
